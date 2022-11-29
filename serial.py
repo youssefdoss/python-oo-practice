a7a93ae0c38f5e70123b943c5db49b70747e616a
Youssef Doss <youssefdoss97@gmail.com>
2022-11-29 18:30:27 -0500

    implement serial and first part of wordfinder

--- serial.py
+++ serial.py
@@ -17,3 +17,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Create serial starting point"""
+        self.start = self.current_value = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.current_value}"
+
+    def generate(self):
+        """Adds 1 to the serial value and returns that value"""
+        self.current_value += 1
+        return self.current_value - 1
+
+    def reset(self):
+        """Resets counter to start value"""
+        self.current_value = self.start
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,27 @@
+from random import choice
+
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    '''Word Finder: finds random words from a dictionary.'''
+
+    def __init__(self, file):
+        '''Creates wordfinder given a file'''
+        self.file = open(file)
+        self.words = self.read_word_by_word()
+
+    
+    def read_word_by_word(self):
+        '''Prints each word of given file and creates a list of those words'''
+        words = []
+
+        with self.file as file:
+            for line in file:
+                for word in line.split():
+
+                    words.append(word)
+
+        print(f'{len(words)} words read')
+        return words
+
+    def random(self):
+        '''Selects a random word from the words list and returns it'''
+        return choice(self.words)

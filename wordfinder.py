9561ddda0158614eca38b1733d42739cf7228010
Maria Juravic <mariajuravic@yahoo.com>
2022-11-29 16:24:53 -0800

    added subclass ready for refactoring

--- wordfinder.py
+++ wordfinder.py
@@ -4,16 +4,16 @@ class WordFinder:
     '''Word Finder: finds random words from a dictionary.'''
 
     def __init__(self, file):
-        '''Creates wordfinder given a file'''
-        self.file = open(file)
-        self.words = self.read_word_by_word()
+        '''Creates WordFinder given a file'''
+        self.file = file
+        self.words = self.store_words()
 
-    
-    def read_word_by_word(self):
-        '''Prints each word of given file and creates a list of those words'''
+
+    def store_words(self):
+        '''Stores each word of given file and creates a list of those words'''
         words = []
 
-        with self.file as file:
+        with open(self.file) as file:
             for line in file:
                 for word in line.split():
 
@@ -25,3 +25,28 @@ class WordFinder:
     def random(self):
         '''Selects a random word from the words list and returns it'''
         return choice(self.words)
+
+
+class RandomWordFinder(WordFinder):
+    '''Inherits from WordFinder. Finds random words from given file but excludes comments'''
+
+    def __init__(self, file):
+        '''Create RandomWordFinder given a file'''
+        self.file = file
+        self.words = self.store_words_ignoring_comments()
+
+    def store_words_ignoring_comments(self):
+        '''Stores each word of a given file in words, excluding comments'''
+        words = []
+
+        with open(self.file) as file:
+            for line in file:
+                if not line[0] == "#":
+                    for word in line.split():
+
+                        words.append(word)
+
+        print(f'{len(words)} words read')
+        return words
+
+
